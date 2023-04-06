0be15653b83fa86ddfb0c5f54c95d5aff7792172
ekaramyan <56231244+ekaramyan@users.noreply.github.com>
2023-04-07 03:05:55 +0400

    craeted new components and fixed styles

---
+++ Aboba-AI/src/Components/AITextAnswer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function AITextAnswer() {
+    //replace here text answer
+    return (
+        <div>AITextAnswer</div>
+    )
+}
---
+++ Aboba-AI/src/Components/AskAI.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function AskAI() {
+    //replace here input and buttons
+  return (
+    <div>AskAI</div>
+  )
+}
